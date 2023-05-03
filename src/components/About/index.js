feaf20b5eb64fa0246291a23e10882fbfa6f28d0
edudallatorre <eduardodallatorre.io@gmail.com>
2023-05-03 21:44:45 +0100

    Last update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,18 @@
 import AnimatedLetters from '../AnimatedLetters/'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const About = () => {
 
-  // const letterClass = "";
+  const letterClass = "";
 
   return (
 
@@ -17,19 +26,36 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.
+            I'm a developer, Brazilian and passionate about programming and design. 
+            I have experience in Systems Analysis and Development, 
+            Business Administration and Graphic Design.
           </p>
           <p align="LEFT">
-            I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
+            I really enjoy teaching what I know and what I've been learning from others.
           </p>
-          <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
-          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+          <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
         </div>
 
       </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,69 +1,70 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
-// import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import AnimatedLetters from '../AnimatedLetters';
-
-
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [
+    'I',
+    '\'m',
+    '',
+    'E',
+    'd', 
+    'u', 
+    'a', 
+    'r', 
+    'd', 
+    'o'
+  ]
+  const jobArray = [
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
 
-    const nameArray = [
-      'I',
-      '\'m',
-      '',
-      'E',
-      'd', 
-      'u', 
-      'a', 
-      'r', 
-      'd', 
-      'o'
-    ]
-    const jobArray = [
-      'D',
-      'e',
-      'v',
-      'e',
-      'l',
-      'o',
-      'p',
-      'e',
-      'r',
-    ]
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
-    useEffect(() => {
-      return setTimeout(() => {
-        setLetterClass('text-animate-hover')
-      }, 4000)
-    }, [])
-  
-
-    return (
-     <div className="container home-page">
-        <div>
-            <h1>
-             <span className={letterClass}>H</span>
-             <span className={`${letterClass} _12`}>i,</span><br />
-            <AnimatedLetters letterClass={letterClass} 
-             strArray={nameArray}
-            idx={15}
-             /><br />
-             <AnimatedLetters letterClass={letterClass}
-             strArray={jobArray} 
-             idx={22}/>
-            {/* <img src={LogoTitle} alt="developer" /> */}
-             <br />
-             </h1>
-             <h2>@dallatorre.dev</h2>
-            <Link to="/contact" className="flag-button">CONTACT ME</Link>
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>@dallatorre.dev</h2>
+          <Link to="/contact" className="flag-button">CONTACT ME</Link>
         </div>
+      </div>
 
-     </div>   
-    )
-
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Home
